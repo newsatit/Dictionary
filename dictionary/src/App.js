7f5cbafcf4441ebe2f9df8130592efe9e9b576bf
Neal Satitsumpun <satitsumpun@wisc.edu>
2019-08-19 20:54:14 +0700

    Create React App + Try Test Component

---
+++ dictionary/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Result from './Result'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Result></Result>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ dictionary/src/Result.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class Result extends Component {
+    render() {
+        return <p>This is the definition</p>
+    }
+}
+
+export default Result;
